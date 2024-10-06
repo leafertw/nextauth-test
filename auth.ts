ed067bc7b9b70364a8a36cc9e52a3d9ee63241b7
craigbossley <leaferdesign@gmail.com>
2024-10-06 15:33:36 +0800

    added Google

--- auth.ts
+++ auth.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
+import Google from "next-auth/providers/google"
 
 import { ensurePlasmicAppUser } from "@plasmicapp/auth-api";
 
@@ -37,7 +38,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         brandColor: "#1E1E1E",
         logo: "/favicon.ico",
     },
-    providers: [GitHub],
+    providers: [GitHub, Google],
 })
 
 /*
